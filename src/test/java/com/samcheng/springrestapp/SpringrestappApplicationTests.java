04bac086da76a52f80a44bc06b799ab1aabfee28
Sam Cheng <56993697+samchenghowing@users.noreply.github.com>
2024-05-10 21:58:50 +0800

    initial commit

---
+++ src/test/java/com/samcheng/springrestapp/SpringrestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samcheng.springrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
