5ff1f22424bd419dab7ba7eb4af0c1c310d6b5ca
samcheng <samchenghowing@gmail.com>
2024-05-15 22:42:31 +0800

    get index page

---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+https://www.youtube.com/watch?v=-mwpoE0x0JQ&t=618s
+
+
+mvn clean
+mvn spring-boot:run
+
---
+++ src/main/java/com/samcheng/springrestapp/HelloController.java
@@ -0,0 +1,21 @@
+package com.samcheng.springrestapp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index(@RequestParam String param) {
+        return "Hello from spring Boot!";
+    }
+
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    
+}
